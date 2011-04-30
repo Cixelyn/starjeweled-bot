f972b2c60f2758eaa1d9084df71ec5da432baac5
Justin Venezuela <jven@jvenezue.la>
2011-04-30 01:26:18 -0400

    Return None only if there are no more valid moves on the board

--- AI/src/StupidBot.py
+++ AI/src/StupidBot.py
@@ -9,6 +9,8 @@ class StupidBot(BotTemplate):
     def __init__(self):
         
         self.state = -1
+        self.checkedLeft = False
+        self.checkedRight = False
         
     ##
     ## Returns the first pair of squares that can be swapped
@@ -21,51 +23,71 @@ class StupidBot(BotTemplate):
         
         self.state = ( self.state + 1 ) % 4
         
-        if ( self.state == 0 ):
-            for y in xrange(0, board.numRows - 1, 1):
-                for x in xrange(0, board.numCols / 2, 1):
-                    swapH = board.swap(x, y, x + 1, y)
-                    if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x + 1, y) ):
-                        return [[x, y], [x + 1, y]]
-                    swapV = board.swap(x, y, x, y + 1)
-                    if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y + 1) ):
-                        return [[x, y], [x, y + 1]]
-            ## nothing found on the left side of the board
-            self.state = 2
-        if ( self.state == 1 ):
-            for y in xrange(board.numRows / 2, board.numRows - 1, 1):
-                for x in xrange(0, board.numCols / 2, 1):
-                    swapH = board.swap(x, y, x + 1, y)
-                    if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x + 1, y) ):
-                        return [[x, y], [x + 1, y]]
-                    swapV = board.swap(x, y, x, y - 1)
-                    if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y - 1) ):
-                        return [[x, y], [x, y - 1]]
-            ## nothing found on the left side of the board
-            self.state = 2
-            
-        if ( self.state == 2 ):
-            for y in xrange(0, board.numRows - 1, 1):
-                for x in xrange(board.numCols - 1, (board.numCols / 2) - 1, -1):
-                    swapH = board.swap(x, y, x - 1, y)
-                    if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x - 1, y) ):
-                        return [[x, y], [x - 1, y]]
-                    swapV = board.swap(x, y, x, y + 1)
-                    if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y + 1) ):
-                        return [[x, y], [x, y + 1]]
-            ## nothing found on the right side of the board
-            self.state = 0
-        if ( self.state == 3 ):
-            for y in xrange(board.numRows / 2, board.numRows - 1, 1):
-                for x in xrange(board.numCols - 1, (board.numCols / 2) - 1, -1):
-                    swapH = board.swap(x, y, x - 1, y)
-                    if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x - 1, y) ):
-                        return [[x, y], [x - 1, y]]
-                    swapV = board.swap(x, y, x, y - 1)
-                    if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y - 1) ):
-                        return [[x, y], [x, y - 1]]
-            ## nothing found on the right side of the board
-            self.state = 0
+        while ( not checkedLeft or not checkedRight ):
+        
+            if ( self.state == 0 ):
+                for y in xrange(0, board.numRows - 1, 1):
+                    for x in xrange(0, board.numCols / 2, 1):
+                        swapH = board.swap(x, y, x + 1, y)
+                        if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x + 1, y) ):
+                            self.checkedLeft = False
+                            self.checkedRight = False
+                            return [[x, y], [x + 1, y]]
+                        swapV = board.swap(x, y, x, y + 1)
+                        if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y + 1) ):
+                            self.checkedLeft = False
+                            self.checkedRight = False
+                            return [[x, y], [x, y + 1]]
+                ## nothing found on the left side of the board
+                self.state = 2
+                self.checkedLeft = True
+                
+            if ( self.state == 1 ):
+                for y in xrange(board.numRows / 2, board.numRows - 1, 1):
+                    for x in xrange(0, board.numCols / 2, 1):
+                        swapH = board.swap(x, y, x + 1, y)
+                        if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x + 1, y) ):
+                            self.checkedLeft = False
+                            self.checkedRight = False
+                            return [[x, y], [x + 1, y]]
+                        swapV = board.swap(x, y, x, y - 1)
+                        if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y - 1) ):
+                            self.checkedLeft = False
+                            self.checkedRight = False
+                            return [[x, y], [x, y - 1]]
+                ## nothing found on the left side of the board
+                self.state = 2
+                self.checkedLeft = True
+                
+            if ( self.state == 2 ):
+                for y in xrange(0, board.numRows - 1, 1):
+                    for x in xrange(board.numCols - 1, (board.numCols / 2) - 1, -1):
+                        swapH = board.swap(x, y, x - 1, y)
+                        if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x - 1, y) ):
+                            return [[x, y], [x - 1, y]]
+                        swapV = board.swap(x, y, x, y + 1)
+                        if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y + 1) ):
+                            return [[x, y], [x, y + 1]]
+                ## nothing found on the right side of the board
+                self.state = 0
+                self.checkedRight = True
+                
+            if ( self.state == 3 ):
+                for y in xrange(board.numRows / 2, board.numRows - 1, 1):
+                    for x in xrange(board.numCols - 1, (board.numCols / 2) - 1, -1):
+                        swapH = board.swap(x, y, x - 1, y)
+                        if ( swapH.hasTripleAt(x, y) or swapH.hasTripleAt(x - 1, y) ):
+                            return [[x, y], [x - 1, y]]
+                        swapV = board.swap(x, y, x, y - 1)
+                        if ( swapV.hasTripleAt(x, y) or swapV.hasTripleAt(x, y - 1) ):
+                            return [[x, y], [x, y - 1]]
+                ## nothing found on the right side of the board
+                self.state = 0
+                self.checkedRight = True
+        
+        ## if we reach here, there are no valid moves on the board
+        
+        return None
 
 
 if __name__ == "__main__":
